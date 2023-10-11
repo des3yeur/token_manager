c74328ae2eab4a5fc52eed1fa158278e53c5e150
gourdy <gourdygautier@gmail.com>
2023-10-11 16:55:43 +0200

    update rust

--- frontend/src/components/blockchains/rust/index.jsx
+++ frontend/src/components/blockchains/rust/index.jsx
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Token } from "../../../middleware/token";
+import { AdminContext } from "../../context/adminProvider";
 
 const Rust = () => {
+  const { admin, setAdmin } = useContext(AdminContext);
   const [rustFields, setRustFields] = useState({
     tokenName: "",
     decimals: 0,
   });
+
+  const config = {
+    headers: {
+      Authorization: `bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2OTY5NDMzOTUsImV4cCI6MTY5Njk0Njk5NSwicm9sZXMiOlsiUk9MRV9TVVBFUl9VU0VSIiwiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoidGVzdEB0ZXN0LmZyIn0.Dzl1frh-8n9eHLhSuVJLeIXB44n5r0feebEVLsslF1yQxKWbF2iMR9TxGWFMO9MaRb7ZraaFCKTcnWLNSu_s924HB3VUnKShn3vMTnPMinlrypL5IJQZFxX8Ms3HmtfLdN8xBSbPD5XuovqW4-4RD1Jzio2KXwRc-GycEptM7Aa5ZS3gmVvnJRMjCBeuw_D1x2vNduEi3xN86OhPvSn7E9y_WPjPvQ_dzgw5KSCf1b_Omulrtjf92o4KDVjQFZfcKhlZ151X4ymfLPeVoClSUozA9njfCKG1GJFay0M3ipi0726lMo5epSv6Xjpxx_uweaTQA0o8_x8Kfv8ZLikw1Q`,
+    },
+  };
+
+  useEffect(() => {
+    // console.log(admin);
+    Token(admin.username, admin.password).then((response) =>
+      setAdmin({ ...admin, token: response.data.token })
+    );
+    console.log(admin);
+  }, []);
 
   const SubmitRust = () => {
     const data = {
@@ -14,7 +31,7 @@ const Rust = () => {
     };
 
     axios
-      .post("/api/blockchain_rusts", data)
+      .post("/api/blockchain_rusts", data, config)
       .then(() => {
         alert("créé");
       })
