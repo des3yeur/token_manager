fe2afba89eb6bbc7802ff7b02b0cdf2b18194089
gourdy <gourdygautier@gmail.com>
2023-10-23 16:54:13 +0200

    update

--- assets/src/pages/home/index.jsx
+++ assets/src/pages/home/index.jsx
@@ -2,15 +2,30 @@ import React from "react";
 
 const Home = () => {
   return (
-    <main className="min-w-screen min-h-screen bg-[url('https://www.coinhouse.com/wp-content/uploads/2019/06/Token_.jpg')] bg-cover bg-no-repeat bg-center">
-      <div className="min-h-screen flex flex-col justify-center items-center">
-        <h1 className="uppercase font-bold text-lg">
-          Bienvenue sur le site Token Manager !
-        </h1>
-        <h1 className="uppercase font-bold">Connectez-vous pour continuer</h1>
-        <a href="http://localhost:8000/login" className="p-2 bg-green-800">
-          Connexion
-        </a>
+    <main className="bg-green95">
+      <div className="  min-h-screen flex flex-col justify-center items-center">
+        <div className="card card-tertiary ">
+          <div className="card-header ">
+            <h1 className="uppercase font-bold text-lg">
+              Bienvenue sur le site Token Manager !
+            </h1>
+          </div>
+          <div className="card-body">
+            <h1 className="card-text">
+              Connectez-vous pour continuer
+            </h1>
+            <div className="flex justify-center d-flex mt-3">
+              <a href={"/login"}>
+              <button
+                className="btn btn-lg mr-2 btn-primary border-dark"
+                type="button"
+              >
+                  <p className="">Connectez vous</p>
+              </button>
+                </a>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
