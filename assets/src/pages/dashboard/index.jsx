10caa8eb79d3f451f5daded87302f66e8f53f1cd
gourdy <gourdygautier@gmail.com>
2023-10-26 16:58:13 +0200

    update login register

--- assets/src/pages/dashboard/index.jsx
+++ assets/src/pages/dashboard/index.jsx
@@ -13,9 +13,16 @@ const Dashboard = () => {
     solidity: false,
   });
 
+  const config = {
+    headers: {
+      Authorization:
+        "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2OTgzMjY4MDksImV4cCI6MTY5ODMzMDQwOSwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6ImFkbWluQHRva2VuLW1hbmFnZXIuZnIifQ.GGzntZStdTP274jJR0VhfO5W1zHe0oZ-6QTAOkwGCnUJ3_mqHORA1Z-OIe-L43bB5uJhzhx_ZR5fUzBL0jIj_vnsp6f7GA8YtuOhLbI9CMGMHYmPrcjJOoq7i4c5zsZAy1qCfwvB8l7FwGONXjCWfUiScW-WCK5-i-U97CcuDyCW3DUM594IUVOs6jhvNpvyil7B5kIO3em7ttBQuiWPCMuYCZ8WMGeDbBrN-p4t9t0yKkcvkC8zVbtjqAcJjgRfSOrx_-NTqZsWicbUsQ7P50riKJKMnXpwdmMV-KkrH9UvZDTBYE7OIcTfCzRhcxtn8su3YNImZI4p3-nlw6bSMw",
+    },
+  };
+
   useEffect(() => {
     axios
-      .get("/api/blockchains")
+      .get("/api/blockchains", config)
       .then((response) => {
         // console.log(response.data["hydra:member"]);
         setBlockchains(response.data["hydra:member"]);
@@ -62,43 +69,45 @@ const Dashboard = () => {
       console.log();
     }
   };
-  
+
   return (
     <>
-    {window.user.roles.includes("ROLE_ADMIN") ? (
-      <div className="bg-green95">
-        <div className="grid grid-cols-3 ">
-          <div className="grid grid-cols-2 text-white95 text-center card card-tertiary">
-            {blockchains.map((blockchain) => {
-              return (
-                <div key={blockchain.id}>
-                  <div
-                    className="inline-flex"
-                    onClick={() => Blockchains(blockchain.blockchain)}
-                  >
-                    {blockchain.symbol}
-                    <img
-                      src={blockchain.logo}
-                      alt=""
-                      width={50}
-                      height={"auto"}
-                    />
+      {window.user.roles.includes("ROLE_ADMIN") ? (
+        <div className="bg-green95">
+          <div className="grid grid-cols-3 ">
+            <div className="grid grid-cols-2 text-white95 text-center card card-tertiary">
+              {blockchains.map((blockchain) => {
+                return (
+                  <div key={blockchain.id}>
+                    <div
+                      className="inline-flex"
+                      onClick={() => Blockchains(blockchain.blockchain)}
+                    >
+                      {blockchain.symbol}
+                      <img
+                        src={blockchain.logo}
+                        alt=""
+                        width={50}
+                        height={"auto"}
+                      />
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
+            <div className="col-span-2 flex justify-center items-center p-8  card card-tertiary">
+              {showBlockchains.solidity ? <Solidity /> : null}
+              {showBlockchains.rust ? <Rust /> : null}
+              {showBlockchains.go ? <Go /> : null}
+              {showBlockchains.ruby ? <Ruby /> : null}
+            </div>
           </div>
-          <div className="col-span-2 flex justify-center items-center p-8  card card-tertiary">
-            {showBlockchains.solidity ? <Solidity /> : null}
-            {showBlockchains.rust ? <Rust /> : null}
-            {showBlockchains.go ? <Go /> : null}
-            {showBlockchains.ruby ? <Ruby /> : null}
-          </div>
-        </div>
 
           <h1>CONTENU ADMIN</h1>
-      </div>
-        ) : null}
+        </div>
+      ) : (
+        <>Contactez l'administrateur pour bénéficier des fonctions</>
+      )}
       <div>
         <Navbar />
       </div>
